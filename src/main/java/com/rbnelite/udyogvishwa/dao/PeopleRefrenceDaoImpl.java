3a45ab37eb1b833457c65e21e9a7c831c9094b82
unknown <budgeanil@gmail.com>
2014-04-10 12:26:47 +0530

    people you may know solved

--- src/main/java/com/rbnelite/udyogvishwa/dao/PeopleRefrenceDaoImpl.java
+++ src/main/java/com/rbnelite/udyogvishwa/dao/PeopleRefrenceDaoImpl.java
@@ -15,20 +15,20 @@ public class PeopleRefrenceDaoImpl extends BaseDao<IntrestAreas> implements
 	@Override
 	@Transactional
 	public List<IntrestAreas> peopleYouMayKnow(String userMail) {
+		String anil="budgeanil@gmail.com";
+		
 		Session session = sessionFactory.openSession();
 		try {
 			
+			/*(
+					"select I.emailId, I.firstName, I.lastName, Pi.profileImage from IntrestAreas Ia, Index I, ProfileImages Pi"
+					+ " where Ia.userMail = I.emailId and I.emailId = Pi.userMail"
+					+ "")*/
+			
+			
 			return session
-					.createQuery(
-							"select I.emailId, I.firstName, I.lastName, Pi.profileImage from IntrestAreas Ia, Index I, ProfileImages Pi"
-							+ " where Ia.userMail = I.emailId and I.emailId = Pi.userMail"
-							+ "")
+					.createQuery("select I.emailId, I.firstName, I.lastName, Pi.profileImage from IntrestAreas Ia, Index I, ProfileImages Pi where Ia.userMail = I.emailId and I.emailId = Pi.userMail and I.emailId not in (select F.requestTo from FriendRequest F  where F.requestFrom = '"+userMail+"' and F.requestStatus = 'Accept' or F.requestStatus = 'Pending') and I.emailId not in (select F.requestFrom from FriendRequest F where F.requestTo = '"+userMail+"' and F.requestStatus = 'Accept' or F.requestStatus = 'Pending')")
 					.list();  
-			/*Query for People you may know once*/
-			/*return session
-					.createQuery(
-							"select I.emailId, I.firstName, I.lastName, Pi.profileImage from Index I, ProfileImages Pi"
-							+ " where I.emailId = Pi.userMail and I.emailId in() ").list();*/
 			
 			
 		} finally {
